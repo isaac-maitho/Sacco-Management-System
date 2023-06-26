473a3b6bd53cc8bca66ac97c7f6e273d41adab67
wyclif-maitho <isaacmaitho14@gmail.com>
2023-06-26 23:59:36 +0300

    Mapped List user

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -25,7 +25,7 @@ const Menu = () => {
     return (
 
             <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-                <a className="navbar-brand" href="index.html">Hospital Management system </a>
+                <a className="navbar-brand" href="index.html">Sacco Management system </a>
                 <button className="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i
                     className="fas fa-bars"></i></button>
                 
